033a798ec20eacf13d5d4eaf57b560f5cb606b10
Hec98 <hector982015@gmail.com>
2021-07-29 12:49:16 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -23,25 +23,3 @@ def menu(title, items):
     num = main_menu.show()
     num += 1
     return num
-
-title = "Menu"
-items = ["1 Opc", 
-         "2 Opc", 
-         "3 Opc", 
-         "Exit"]
-
-repeat = True
-while repeat:
-    opc = menu(title, items)
-
-    if (opc == 1):
-        print('Op 1')
-        pausa()
-    elif (opc == 2):
-        print('Op 2')
-        pausa()
-    elif (opc == 3):
-        print('Op 3')
-        pausa()
-
-    repeat = (opc < len(items))
